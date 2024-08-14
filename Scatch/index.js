afbdbc9006204222b84856396c0776ab2378b63f
ebrahim afridi <afridiebr@student.iul.ac.in>
2024-08-15 00:28:39 +0530

    add routers

--- Scatch/index.js
+++ Scatch/index.js
@@ -2,14 +2,22 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const path = require("path");
 const app = express();
+const db = require("./config/mongoose-connection");
 const port = 3000;
 
+// Middlewares
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
+// Routes + Controllers
+app.use("/owners", ownersRouter);
+app.use("/users", usersRouter);
+app.use("/products", productsRouter);
+
+// Routes
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
